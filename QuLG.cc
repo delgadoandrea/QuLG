5ea9f11b3a08e983f2ae0e12c34908c0eb301f1d
Blaine <baheffron@gmail.com>
2020-06-16 15:55:36 -0400

    adding orthopositronium physics module

--- QuLG.cc
+++ QuLG.cc
@@ -1,9 +1,13 @@
+#include <G4Positron.hh>
+#include <G4eplusAnnihilation.hh>
+#include <OrthoPositronium.hh>
 #include "G4Types.hh"
 #include "G4RunManager.hh"
 #include "G4UImanager.hh"
 #include "G4String.hh"
 
 #include "FTFP_BERT.hh"
+#include "QGSP_BERT_HP.hh"
 #include "G4OpticalPhysics.hh"
 #include "G4EmStandardPhysics_option4.hh"
 
@@ -26,8 +30,15 @@ int main(int argc, char** argv)
   QuLGDetectorConstruction* det = new QuLGDetectorConstruction();
   runManager->SetUserInitialization(det);
 
-  G4VModularPhysicsList* physicsList = new FTFP_BERT; //TODO Compare to PROSPECT's Physics List
+  // G4VModularPhysicsList* physicsList = new FTFP_BERT; //TODO Compare to PROSPECT's Physics List
+  G4VModularPhysicsList* physicsList = new QGSP_BERT_HP; // PG4 physics list 
   physicsList->ReplacePhysics(new G4EmStandardPhysics_option4());
+  // G4ProductionCutsTable::GetProductionCutsTable()->SetEnergyRange(250*eV, 1*GeV);
+  // double rangecut = 1*um;
+  // SetCutValue(rangecut, "gamma");
+  // SetCutValue(rangecut, "e-");
+  // SetCutValue(rangecut, "e+");
+  
   G4OpticalPhysics* opticalPhysics = new G4OpticalPhysics();
   opticalPhysics->SetWLSTimeProfile("delta");
 
@@ -41,6 +52,26 @@ int main(int argc, char** argv)
   opticalPhysics->SetTrackSecondariesFirst(kScintillation, true);
 
   physicsList->RegisterPhysics(opticalPhysics);
+  bool useOrthoPositronium = true;
+    if(useOrthoPositronium) {
+        //OrthoPositronium module
+        // positron definition
+        auto posDef = G4Positron::Positron();
+        G4ProcessManager *pmanager = posDef->GetProcessManager();
+        auto pos_pl = pmanager->GetProcessList();      // locate annihilation process for positron
+        G4eplusAnnihilation *posep = nullptr;
+        for (decltype(pos_pl->size()) i = 0; i < pos_pl->size() && !posep; ++i) {
+            posep = dynamic_cast<G4eplusAnnihilation *>((*pos_pl)[i]);
+        }
+        if (!posep)
+            throw std::logic_error(
+                    "Positron annihilation process missing, make sure you are using the standard Em Model (use macro command /phys/enableEM Standard )");
+        pmanager->RemoveProcess(posep);
+        pmanager->AddProcess(new OrthoPositronium,
+                             0, -1, 4);
+    }
+  ////////////////////////////////////////////////
+
   runManager->SetUserInitialization(physicsList);
 
   runManager->SetUserInitialization(new QuLGActionInitialization(det));
